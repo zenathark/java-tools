5844b1fefde59d3864498f6f6d499e6851a2439c
Juan Carlos Galan Hernandez <jcgalanhlanh@gmail.com>
2018-02-05 13:05:30 -0600

    Add traversal signatures

--- src/main/java/FastBST.java
+++ src/main/java/FastBST.java
@@ -10,16 +10,16 @@ public class FastBST {
   public FastBST left;
   public FastBST parent;
   public int label;
-  //Payload
+  // Payload
 
   public FastBST(int label) {
     this(label, null, null, null);
   }
 
   public FastBST(int label, FastBST left, FastBST right, FastBST parent) {
-    this.label  = label;
-    this.left   = left;
-    this.right  = right;
+    this.label = label;
+    this.left = left;
+    this.right = right;
     this.parent = parent;
   }
 
@@ -27,36 +27,105 @@ public class FastBST {
     return label;
   }
 
+  /** Simple representation of the node. Mostly used for debugging. */
   public String toString() {
-    String l = (left == null)? "" : left.toString();
-    String r = (right == null)? "" : right.toString();
-    String p = (parent == null)? "" : parent.toString();
+    String l = (left == null) ? "" : left.toString();
+    String r = (right == null) ? "" : right.toString();
+    String p = (parent == null) ? "" : parent.toString();
     return String.format("{%d, L: %s, R: %s, P: %s}", label, l, r, p);
   }
 
-  public static class BinaryTreeFn {
-
-    public static FastBST insert(FastBST root, FastBST newNode) {
-      FastBST cNode = root;
-      while (true) {
-        if (newNode.getLabel() < cNode.getLabel()) {
-          if (cNode.left != null)
-            cNode = cNode.left;
-          else {
-            cNode.left = newNode;
-            break;
-          }
-        } else {
-          if (cNode.right != null)
-            cNode = cNode.right;
-          else {
-            cNode.right = newNode;
-            break;
-          }
+  /**
+   * Inserts a new leaf into the tree.
+   *
+   * @param root Root of the BST tree.
+   * @param newNode Node to be inserted as a leaf.
+   */
+  public static FastBST insert(FastBST root, FastBST newNode) {
+    FastBST cNode = root;
+    while (true) {
+      if (newNode.getLabel() < cNode.getLabel()) {
+        if (cNode.left != null) cNode = cNode.left;
+        else {
+          cNode.left = newNode;
+          break;
         }
+      } else if (newNode.getLabel() > cNode.getLabel()) {
+        if (cNode.right != null) cNode = cNode.right;
+        else {
+          cNode.right = newNode;
+          break;
+        }
+      } else { // Same labels are not allowed
+        break;
       }
-      return root;
     }
-
+    return root;
   }
+
+  public static FastBST search(FastBST root, int label) {
+    FastBST ans = root;
+    while (ans != null) {
+      if (ans.label == label) break;
+      else if (ans.label < label) {
+        ans = ans.right;
+        break;
+      } else {
+        ans = ans.left;
+        break;
+      }
+    }
+    return ans;
+  }
+
+  /**
+   * Remove a node. This function removes a node and replaces it with is in-order predecessor.
+   *
+   * @param root BST's root
+   * @param label Node's label to be removed
+   */
+  public static FastBST remove(FastBST root, int label) {
+    FastBST d = search(root, label);
+    if (d.left == null && d.right == null) {
+      if (d.parent.left.label == d.label) d.parent.left = null;
+      else d.parent.right = null;
+
+    } else if (d.left == null && d.right != null) {
+      if (d.parent.left.label == d.label) d.parent.left = d.right;
+      else d.parent.right = d.right;
+
+    } else if (d.left != null && d.right == null) {
+      if (d.parent.left.label == d.label) d.parent.left = d.left;
+      else d.parent.right = d.left;
+
+    } else {
+      FastBST rep = d.left;
+      while (rep.right != null) rep = rep.right;
+      if (d.parent.left.label == d.label) d.parent.left = rep;
+      else d.parent.right = rep;
+    }
+    return root;
+  }
+
+  public static FastBST rotateLeft(FastBST root, FastBST pivot) {
+    FastBST parent = pivot.parent;
+    parent.right = pivot.left;
+    pivot.left = parent;
+    return root;
+  }
+
+  public static FastBST rotateRight(FastBST root, FastBST pivot) {
+    FastBST parent = pivot.parent;
+    parent.left = pivot.right;
+    pivot.right = parent;
+    return root;
+  }
+
+  public class PreOrder implements Iterator<FastBST> {}
+
+  public class InOrder implements Iterator<FastBST> {}
+
+  public class PostOrder implements Iterator<FastBST> {}
+
+  public class LevelOrder implements Iterator<FastBST> {}
 }
