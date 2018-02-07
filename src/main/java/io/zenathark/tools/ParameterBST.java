59220c0ad733ab88629f1ee68d92eafb70489f1d
Zenathark <jcgalanh@gmail.com>
2018-02-07 12:40:00 -0600

    Added parameterized BST

--- src/main/java/io/zenathark/tools/ParameterBST.java
+++ src/main/java/io/zenathark/tools/ParameterBST.java
@@ -7,30 +7,30 @@ import java.math.*;
 import java.util.regex.*;
 import java.util.function.*;
 
-public class FastBST {
-  public FastBST right;
-  public FastBST left;
-  public FastBST parent;
-  public int label;
-  // Payload
+public class ParameterBST<T extends Comparable<T>> {
+  public ParameterBST<T> right;
+  public ParameterBST<T> left;
+  public ParameterBST<T> parent;
+  public T payload;
 
-  public static FastBST createRoot(int label) {
-    return new FastBST(label);
+  public static <T extends Comparable<T>> ParameterBST<T> createRoot(T payload) {
+    return new ParameterBST<>(payload);
   }
 
-  private FastBST(int label) {
-    this(label, null, null, null);
+  private ParameterBST(T payload) {
+    this(payload, null, null, null);
   }
 
-  private FastBST(int label, FastBST left, FastBST right, FastBST parent) {
-    this.label = label;
+  private ParameterBST(
+      T payload, ParameterBST<T> left, ParameterBST<T> right, ParameterBST<T> parent) {
+    this.payload = payload;
     this.left = left;
     this.right = right;
     this.parent = parent;
   }
 
-  public int getLabel() {
-    return label;
+  public T getPayload() {
+    return payload;
   }
 
   /** Simple representation of the node. Mostly used for debugging. */
@@ -38,11 +38,11 @@ public class FastBST {
     String l = (left == null) ? "" : left.toString();
     String r = (right == null) ? "" : right.toString();
     String p = (parent == null) ? "" : parent.toString();
-    return String.format("{%d, L: %s, R: %s, P: %s}", label, l, r, p);
+    return String.format("{%d, L: %s, R: %s, P: %s}", payload, l, r, p);
   }
 
-  public static FastBST insert(FastBST root, int label) {
-    return insert(root, new FastBST(label));
+  public static <T extends Comparable<T>> ParameterBST<T> insert(ParameterBST<T> root, T payload) {
+    return insert(root, new ParameterBST<>(payload));
   }
   /**
    * Inserts a new leaf into the tree.
@@ -50,34 +50,35 @@ public class FastBST {
    * @param root Root of the BST tree.
    * @param newNode Node to be inserted as a leaf.
    */
-  public static FastBST insert(FastBST root, FastBST newNode) {
+  public static <T extends Comparable<T>> ParameterBST<T> insert(
+      ParameterBST<T> root, ParameterBST<T> newNode) {
     if (root == null) return newNode;
-    FastBST cNode = root;
+    ParameterBST<T> cNode = root;
     while (true) {
-      if (newNode.getLabel() < cNode.getLabel()) {
+      if (newNode.getPayload().compareTo(cNode.getPayload()) < 0) {
         if (cNode.left != null) cNode = cNode.left;
         else {
           cNode.left = newNode;
           break;
         }
-      } else if (newNode.getLabel() > cNode.getLabel()) {
+      } else if (newNode.getPayload().compareTo(cNode.getPayload()) > 0) {
         if (cNode.right != null) cNode = cNode.right;
         else {
           cNode.right = newNode;
           break;
         }
-      } else { // Same labels are not allowed
+      } else { // Same payloads are not allowed
         break;
       }
     }
     return root;
   }
 
-  public static FastBST search(FastBST root, int label) {
-    FastBST ans = root;
+  public static <T extends Comparable<T>> ParameterBST<T> search(ParameterBST<T> root, T payload) {
+    ParameterBST<T> ans = root;
     while (ans != null) {
-      if (ans.label == label) break;
-      else if (ans.label < label) {
+      if (ans.payload == payload) break;
+      else if (ans.payload.compareTo(payload) < 0) {
         ans = ans.right;
         break;
       } else {
@@ -92,72 +93,74 @@ public class FastBST {
    * Remove a node. This function removes a node and replaces it with is in-order predecessor.
    *
    * @param root BST's root
-   * @param label Node's label to be removed
+   * @param payload Node's payload to be removed
    */
-  public static FastBST remove(FastBST root, int label) {
-    FastBST d = search(root, label);
+  public static <T extends Comparable<T>> ParameterBST<T> remove(ParameterBST<T> root, T payload) {
+    ParameterBST<T> d = search(root, payload);
     if (d.left == null && d.right == null) {
-      if (d.parent.left.label == d.label) d.parent.left = null;
+      if (d.parent.left.payload == d.payload) d.parent.left = null;
       else d.parent.right = null;
 
     } else if (d.left == null && d.right != null) {
-      if (d.parent.left.label == d.label) d.parent.left = d.right;
+      if (d.parent.left.payload == d.payload) d.parent.left = d.right;
       else d.parent.right = d.right;
 
     } else if (d.left != null && d.right == null) {
-      if (d.parent.left.label == d.label) d.parent.left = d.left;
+      if (d.parent.left.payload == d.payload) d.parent.left = d.left;
       else d.parent.right = d.left;
 
     } else {
-      FastBST rep = d.left;
+      ParameterBST<T> rep = d.left;
       while (rep.right != null) rep = rep.right;
-      if (d.parent.left.label == d.label) d.parent.left = rep;
+      if (d.parent.left.payload == d.payload) d.parent.left = rep;
       else d.parent.right = rep;
     }
     return root;
   }
 
-  public static FastBST rotateLeft(FastBST root, FastBST pivot) {
-    FastBST parent = pivot.parent;
+  public static <T extends Comparable<T>> ParameterBST<T> rotateLeft(
+      ParameterBST<T> root, ParameterBST<T> pivot) {
+    ParameterBST<T> parent = pivot.parent;
     parent.right = pivot.left;
     pivot.left = parent;
     return root;
   }
 
-  public static FastBST rotateRight(FastBST root, FastBST pivot) {
-    FastBST parent = pivot.parent;
+  public static <T extends Comparable<T>> ParameterBST<T> rotateRight(
+      ParameterBST<T> root, ParameterBST<T> pivot) {
+    ParameterBST<T> parent = pivot.parent;
     parent.left = pivot.right;
     pivot.right = parent;
     return root;
   }
 
-  public Iterable<FastBST> preorderIterator() {
-    return new Iterable<FastBST>() {
-      public Iterator<FastBST> iterator() {
-        return new PreOrder(FastBST.this);
+  public Iterable<ParameterBST<T>> preorderIterator() {
+    return new Iterable<ParameterBST<T>>() {
+      public Iterator<ParameterBST<T>> iterator() {
+        return new PreOrder(ParameterBST.this);
       }
     };
   }
 
   // Static pre order traversal, only for hard code editing
   // This is not faster than the iterator, see benchmark
-  public static void preOrder(FastBSTp root) {
-    ArrayDeque<FastBSTp> queue = new ArrayDeque<>();
+  public static <T extends Comparable<T>> void preOrder(ParameterBST<T> root) {
+    ArrayDeque<ParameterBST<T>> queue = new ArrayDeque<>();
     queue.addLast(root);
     while (!queue.isEmpty()) {
-      FastBSTp ans = queue.removeFirst();
+      ParameterBST<T> ans = queue.removeFirst();
       // Test function
-      int t = ans.label + 5;
+      // int t = ans.payload + 5;
       // End Test Function
       if (ans.left != null) queue.addFirst(ans.left);
       if (ans.right != null) queue.addFirst(ans.right);
     }
   }
 
-  class PreOrder implements Iterator<FastBST> {
-    ArrayDeque<FastBST> queue;
+  class PreOrder implements Iterator<ParameterBST<T>> {
+    ArrayDeque<ParameterBST<T>> queue;
 
-    public PreOrder(FastBST root) {
+    public PreOrder(ParameterBST<T> root) {
       queue = new ArrayDeque<>();
       queue.addLast(root);
     }
@@ -168,19 +171,19 @@ public class FastBST {
     }
 
     @Override
-    public FastBST next() {
-      FastBST ans = queue.removeFirst();
+    public ParameterBST<T> next() {
+      ParameterBST<T> ans = queue.removeFirst();
       if (ans.left != null) queue.addFirst(ans.left);
       if (ans.right != null) queue.addFirst(ans.right);
       return ans;
     }
   }
 
-  class InOrder implements Iterator<FastBST> {
-    ArrayDeque<FastBST> queue;
-    FastBST cNode;
+  class InOrder implements Iterator<ParameterBST<T>> {
+    ArrayDeque<ParameterBST<T>> queue;
+    ParameterBST<T> cNode;
 
-    public InOrder(FastBST root) {
+    public InOrder(ParameterBST<T> root) {
       queue = new ArrayDeque<>();
       cNode = root;
     }
@@ -191,8 +194,8 @@ public class FastBST {
     }
 
     @Override
-    public FastBST next() {
-      FastBST ans;
+    public ParameterBST<T> next() {
+      ParameterBST<T> ans;
       while (cNode != null) {
         queue.addFirst(cNode);
         cNode = cNode.left;
@@ -203,11 +206,11 @@ public class FastBST {
     }
   }
 
-  class PostOrder implements Iterator<FastBST> {
-    ArrayDeque<FastBST> queue;
-    FastBST cNode;
+  class PostOrder implements Iterator<ParameterBST<T>> {
+    ArrayDeque<ParameterBST<T>> queue;
+    ParameterBST<T> cNode;
 
-    public PostOrder(FastBST root) {
+    public PostOrder(ParameterBST<T> root) {
       queue = new ArrayDeque<>();
       cNode = root;
     }
@@ -218,8 +221,8 @@ public class FastBST {
     }
 
     @Override
-    public FastBST next() {
-      FastBST ans;
+    public ParameterBST<T> next() {
+      ParameterBST<T> ans;
       while (cNode != null) {
         queue.addFirst(cNode);
         cNode = cNode.left;
@@ -230,11 +233,11 @@ public class FastBST {
     }
   }
 
-  class LevelOrder implements Iterator<FastBST> {
-    ArrayDeque<FastBST> queue;
-    FastBST cNode;
+  class LevelOrder implements Iterator<ParameterBST<T>> {
+    ArrayDeque<ParameterBST<T>> queue;
+    ParameterBST<T> cNode;
 
-    public LevelOrder(FastBST root) {
+    public LevelOrder(ParameterBST<T> root) {
       queue = new ArrayDeque<>();
       cNode = root;
     }
@@ -245,8 +248,8 @@ public class FastBST {
     }
 
     @Override
-    public FastBST next() {
-      FastBST ans;
+    public ParameterBST<T> next() {
+      ParameterBST<T> ans;
       while (cNode != null) {
         queue.addFirst(cNode);
         cNode = cNode.left;
