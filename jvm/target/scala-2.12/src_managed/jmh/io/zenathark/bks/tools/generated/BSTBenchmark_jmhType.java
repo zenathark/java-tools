edb4261b6960216f0689daf3303d0cb560823da8
Zenathark <jcgalanh@gmail.com>
2018-02-19 11:18:54 -0600

    Moved java tools to its own directory

---
+++ jvm/target/scala-2.12/src_managed/jmh/io/zenathark/bks/tools/generated/BSTBenchmark_jmhType.java
@@ -0,0 +1,4 @@
+package io.zenathark.bks.tools.generated;
+public class BSTBenchmark_jmhType extends BSTBenchmark_jmhType_B3 {
+}
+
---
+++ jvm/target/scala-2.12/src_managed/jmh/io/zenathark/bks/tools/generated/ParameterBSTBenchmark_jmhType.java
@@ -0,0 +1,4 @@
+package io.zenathark.bks.tools.generated;
+public class ParameterBSTBenchmark_jmhType extends ParameterBSTBenchmark_jmhType_B3 {
+}
+
